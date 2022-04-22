e70ee7ff60ca5e3aca9c9adc160e19ca41039ef6
tvk0078 <78614451+tvk0078@users.noreply.github.com>
2022-04-22 10:45:35 +0530

    Add files via upload

---
+++ JavaPractice/src/com/company/Lambdas/Arrays.java
@@ -0,0 +1,14 @@
+package com.company.Lambdas;
+
+import java.util.ArrayList;
+
+public class Arrays {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        numbers.add(5);
+        numbers.add(9);
+        numbers.add(8);
+        numbers.add(1);
+        numbers.forEach( (n) -> { System.out.println(n); } );
+    }
+}
